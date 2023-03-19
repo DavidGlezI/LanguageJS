70fb2e132f6fb74c3f74640ef590242c75880b23
DavidGlezI <88803550+DavidGlezI@users.noreply.github.com>
2023-03-19 14:00:34 -0600

    yes?

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,9 +1,9 @@
+
 /*
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
 */
-
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 canvas.height = 400;
@@ -18,14 +18,51 @@ img1.onload = function(){
     ctx.drawImage(img1, xPos,yPos, 100 ,100); 
 }
 
-function moveUfo(x,y, repeat){
-    for(let i =0; i<repeat;i++){
-        ctx.clearRect(xPos,yPos,xPos+ 100 ,yPos + 100 );
-        xPos += x;
-        yPos += y;
-        ctx.drawImage(img1, xPos,yPos, 100 ,100); 
+async function moveUfo(x,y){
+    const asyncWait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+    xPos = Number(xPos);
+    x = Number(x);
+    x= xPos+ x;
+    console.log(x);
+    while (true){
+        if(xPos < x){
+            xPos++;
+        }
+        else if (xPos > x){
+            
+            xPos--
+        }
+        ctx.drawImage(img1, xPos,yPos, 100 ,100);
+        if(xPos === x){
+            break;
+        }
+        
+        await asyncWait(10);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
-    
+
+    yPos = Number(yPos);
+    y = Number(y);
+    y= yPos+ y;
+    console.log(y);
+    while (true){
+        if(yPos < y){
+            yPos++;
+        }
+        else if (yPos > y){
+            
+            yPos--
+        }
+        ctx.drawImage(img1, xPos,yPos, 100 ,100);
+        if(yPos === x){
+            break;
+        }
+        
+        await asyncWait(10);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    return Promise.resolve(undefined);
 }
 
 let btnAdelante = document.getElementById("adelante");
@@ -33,57 +70,247 @@ let btnAtras = document.getElementById("atras");
 let btnIzquierda = document.getElementById("izquierda");
 let btnDerecha = document.getElementById("derecha");
 
+let btnSubmit = document.getElementById('submit');
 
-function adelante(repeat){
+let string="";
+
+btnSubmit.addEventListener('click',()=>{
+    let x = document.getElementById("text").value;
+    string = x;
+    document.getElementById("text").value = " ";
+    console.log(string);
+    logica(string);
+    
+})
+
+
+
+
+function adelante(numMovimiento){
     ctx.beginPath();
     ctx.moveTo(xPos,yPos);
-    moveUfo(0,-10, repeat);
+    moveUfo(0,-numMovimiento);
+}
+
+function atras(numMovimiento){
+    ctx.beginPath();
+    ctx.moveTo(xPos,yPos);
+    moveUfo(0,numMovimiento);
 
 }
 
-function atras(repeat){
-    ctx.beginPath();
-    ctx.moveTo(xPos,yPos);
-    moveUfo(0,10, repeat);
+async function izquierda(numMovimiento){
+    console.log(numMovimiento);
+    console.log("izquierda");
+    await moveUfo(-numMovimiento,0);
 
 }
 
-function izquierda(repeat){
-    ctx.beginPath();
-    ctx.moveTo(xPos,yPos);
-    moveUfo(-10,0, repeat);
+async function derecha(numMovimiento){
+    console.log(numMovimiento);
+    console.log("derecha");
+    await moveUfo(numMovimiento,0);
 
 }
 
-function derecha(repeat){
-    ctx.beginPath();
-    ctx.moveTo(xPos,yPos);
-    moveUfo(10,0, repeat);
 
-}
-
-btnAdelante.addEventListener("click",()=>{
-    //   
-    adelante(3);
-})
-
-btnAtras.addEventListener("click",()=>{
-    //   
-    atras();
-})
-
-btnIzquierda.addEventListener("click",()=>{
-    //   
-    izquierda();
-})
-
-btnDerecha.addEventListener("click",()=>{
-    //   
-    derecha();
-})
 
 
 
 function limpiarCanvas(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+
+
+function logica(input){
+    /*
+    Avanzar, Retroceder, Repetir, Vuelta,  Centrar, Pintura
+
+    Avanzar (10)
+    Retrocer (10) Repetir (3)
+    Vuelta izquierda 60
+    Centrar ovni 
+    Pintura roja
+    */
+
+
+
+
+    const regex = /[\w]+|[\(\)]|[0-9]*/g; // Hacer una expresion que acepte todo tipo de palabras, caracteres y simbolos.
+    const found = input.match(regex); // Guarda en un arreglo todas las palabras que hicieron match con la regex
+
+
+    // Map con las expresiones y sus tokens correspondientes
+    const expressions= new Map([
+        [/[aA]vanzar|[rR]etroceder|[rR]epetir|[vV]uelta|[cC]entrar|[Pp]intura/, "Instruccion"],
+        [/\(/, "inicioParentesis"],  // value = regex, key = string
+        [/\)/, "finalParentesis"],
+        [/[Ii]zquierda|[Dd]erecha/, "direccion"],
+        [/[Rr]ojo|[Aa]zul|[Vv]erde|[Aa]marillo/, "color"],
+        [/[Oo]vni/, "objeto"],
+        [/^(?:[1-9]|[1-4][0-9]|50)$/, "numero"],
+        [/([0-2]?[0-9]{1,2}|3[0-5][0-9]|360)/, "grados"],
+    ]);
+        
+
+
+        
+    //Cada palabra del arreglo se checan con el primer valor del map, que son las expresiones regulares
+    // Si hacen match, se guarda el token en otro arreglo
+
+    // Quitamos espacios en blanco del arreglo
+    found.forEach((word,i)=>{
+        if(word === ''){
+            found.splice(i,1);
+        }
+    })
+
+    expressions.forEach((key, value)=>{
+        let regExp=(value);
+        
+        found.forEach((word,i)=>{
+            if(regExp.test(word)){
+                found[i]= {key,word};
+            }
+        })
+
+    })
+
+    console.log(found);
+
+    function sigToken(){
+        let token = found.shift();
+        return [token.key, token.word];
+    }
+
+    function verificarInputVacio(){ // Utilizamos esta función para revisar si todavía hay items en el arreglo con las palabras aceptadas
+        if(found.length === 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    
+
+    async function parser(){
+        let [token,word] = sigToken();
+            if(token === 'Instruccion'){
+                let mov = word;
+                [token,word] = sigToken();
+                if(token === 'color' | token === 'objeto'){ // Checamos la constante (color, objeto, direccion) para aceptarla y terminar ó aceptarla y buscar más expresiones
+                    // Si el arreglo todavía tiene algo, llamar a recursividad para aceptar la siguiente expresión
+                    if(verificarInputVacio()){
+                        console.log("Frase aceptada / Sintaxis correcta");
+                        //process.exit(0);
+                    }
+                    else{
+                        parser();
+                    }
+                }
+                else if(token === 'direccion'){
+                    let dir = word;
+                    [token, word] = sigToken();
+                    if(token === 'inicioParentesis' ){ // buscamos aceptar el (
+                        [token, word] = sigToken();
+                        if(  token === 'numero' | token === 'grado'){
+                            let num = word;
+                            [token,word] = sigToken();
+                            if(token === 'finalParentesis'){
+                                if(verificarInputVacio()){
+                                    console.log("Frase aceptada / Sintaxis correcta");
+                                    
+                                    if(dir === 'izquierda'){
+                                        await izquierda(num);
+                                    }
+                                    else if(dir === 'derecha'){
+                                        await derecha(num);
+                                    }
+                                    //process.exit(0); // Termina el programa exitosamente
+                                }
+                                else{
+                                    
+                                    //[token,word] = sigToken();
+                                    parser();
+                                    console.log(dir);
+                                    console.log(num);
+                                    if(dir === 'izquierda'){
+                                        await izquierda(num);
+                                    }
+                                    else if(dir === 'derecha'){
+                                        await derecha(num);
+                                    }
+                                }
+                            }
+                            else{
+                                console.log("Error! Se esperaba: )");
+                                //process.exit(1); // Termina el programa con error
+                            }
+                        }
+                        else{
+                            console.log("Error! Se esperaba: numero: 0-360");
+                            //process.exit(1); // Termina el programa con error
+                        }
+                    }
+                    else{
+                        console.log("Error! Se esperaba: (");
+                        //process.exit(1); // Termina el programa con error
+                    }
+                }
+                else if(token === 'inicioParentesis' ){ // buscamos aceptar el (
+                    [token,word] = sigToken();
+                    if(  token === 'numero' | token === 'grado'){
+                        let num = word;
+                        [token,word] = sigToken();
+                        if(token === 'finalParentesis'){
+                            if(verificarInputVacio()){
+                                console.log("Frase aceptada / Sintaxis correcta");
+                                if(mov = 'avanzar'){
+                                    await adelante(num);
+                                }
+                                else if(mov = 'retroceder'){
+                                    await atras(num);
+                                }
+                                //process.exit(0); // Termina el programa exitosamente
+                            }
+                            else{
+                                if(mov = 'avanzar'){
+                                    adelante(num);
+                                }
+                                else if(mov = 'retroceder'){
+                                    atras(num);
+                                }
+                                //[token,word] = sigToken();
+                                parser();
+                            }
+                        }
+                        else{
+                            console.log("Error! Se esperaba: )");
+                            //process.exit(1); // Termina el programa con error
+                        }
+                    }
+                    else{
+                        console.log("Error! Se esperaba: numero: 0-360");
+                        //process.exit(1); // Termina el programa con error
+
+                    }
+
+                }
+                else{
+                    console.log("Error! Se esperaba: color: rojo/azul/amarillo/verde ó objeto: ovni ó direccion: izquierda/derecha ó (");
+                    //process.exit(1); // Termina el programa con error
+                }
+            }
+            else{
+                console.log("Error! Se esperaba: Instruccion");
+                //process.exit(1);
+            } 
+        }
+
+
+    parser();
+}
+
+
---
+++ Frontend/logica.js
@@ -0,0 +1,160 @@
+
+function logica(input){
+    /*
+    Avanzar, Retroceder, Repetir, Vuelta,  Centrar, Pintura
+
+    Avanzar (10)
+    Retrocer (10) Repetir (3)
+    Vuelta izquierda 60
+    Centrar ovni 
+    Pintura roja
+    */
+
+
+
+
+    const regex = /[\w]+|[\(\)]|[0-9]*/g; // Hacer una expresion que acepte todo tipo de palabras, caracteres y simbolos.
+    const found = input.match(regex); // Guarda en un arreglo todas las palabras que hicieron match con la regex
+
+
+    // Map con las expresiones y sus tokens correspondientes
+    const expressions= new Map([
+        [/[aA]vanzar|[rR]etroceder|[rR]epetir|[vV]uelta|[cC]entrar|[Pp]intura/, "Instruccion"],
+        [/\(/, "inicioParentesis"],  // value = regex, key = string
+        [/\)/, "finalParentesis"],
+        [/[Ii]zquierda|[Dd]erecha/, "direccion"],
+        [/[Rr]ojo|[Aa]zul|[Vv]erde|[Aa]marillo/, "color"],
+        [/[Oo]vni/, "objeto"],
+        [/^(?:[1-9]|[1-4][0-9]|50)$/, "numero"],
+        [/([0-2]?[0-9]{1,2}|3[0-5][0-9]|360)/, "grados"],
+    ]);
+        
+
+
+        
+    //Cada palabra del arreglo se checan con el primer valor del map, que son las expresiones regulares
+    // Si hacen match, se guarda el token en otro arreglo
+
+    // Quitamos espacios en blanco del arreglo
+    found.forEach((word,i)=>{
+        if(word === ''){
+            found.splice(i,1);
+        }
+    })
+
+    expressions.forEach((key, value)=>{
+        let regExp=(value);
+        
+        found.forEach((word,i)=>{
+            if(regExp.test(word)){
+                found[i]= key;
+            }
+            
+        })
+
+
+    })
+
+
+    function sigToken(){
+        let token = found.shift();
+        return token;
+    }
+
+    function verificarInputVacio(){ // Utilizamos esta función para revisar si todavía hay items en el arreglo con las palabras aceptadas
+        if(found.length === 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    let token = sigToken();
+    function parser(){
+            if(token === 'Instruccion'){
+                token = sigToken();
+                if(token === 'color' | token === 'objeto'){ // Checamos la constante (color, objeto, direccion) para aceptarla y terminar ó aceptarla y buscar más expresiones
+                    // Si el arreglo todavía tiene algo, llamar a recursividad para aceptar la siguiente expresión
+                    if(verificarInputVacio()){
+                        console.log("Frase aceptada / Sintaxis correcta");
+                        process.exit(0);
+                    }
+                    else{
+                        token = sigToken();
+                        parser();
+                    }
+                }
+                else if(token === 'direccion'){
+                    token = sigToken();
+                    if(token === 'inicioParentesis' ){ // buscamos aceptar el (
+                        token = sigToken();
+                        if(  token === 'numero' | token === 'grado'){
+                            token = sigToken();
+                            if(token === 'finalParentesis'){
+                                if(verificarInputVacio()){
+                                    console.log("Frase aceptada / Sintaxis correcta");
+                                    process.exit(0); // Termina el programa exitosamente
+                                }
+                                else{
+                                    token = sigToken();
+                                    parser();
+                                }
+                            }
+                            else{
+                                console.log("Error! Se esperaba: )");
+                                process.exit(1); // Termina el programa con error
+                            }
+                        }
+                        else{
+                            console.log("Error! Se esperaba: numero: 0-360");
+                            process.exit(1); // Termina el programa con error
+                        }
+                    }
+                    else{
+                        console.log("Error! Se esperaba: (");
+                        process.exit(1); // Termina el programa con error
+                    }
+                }
+                else if(token === 'inicioParentesis' ){ // buscamos aceptar el (
+                    token = sigToken();
+                    if(  token === 'numero' | token === 'grado'){
+                        token = sigToken();
+                        if(token === 'finalParentesis'){
+                            if(verificarInputVacio()){
+                                console.log("Frase aceptada / Sintaxis correcta");
+                                process.exit(0); // Termina el programa exitosamente
+                            }
+                            else{
+                                token = sigToken();
+                                parser();
+                            }
+                        }
+                        else{
+                            console.log("Error! Se esperaba: )");
+                            process.exit(1); // Termina el programa con error
+                        }
+                    }
+                    else{
+                        console.log("Error! Se esperaba: numero: 0-360");
+                        process.exit(1); // Termina el programa con error
+
+                    }
+
+                }
+                else{
+                    console.log("Error! Se esperaba: color: rojo/azul/amarillo/verde ó objeto: ovni ó direccion: izquierda/derecha ó (");
+                    process.exit(1); // Termina el programa con error
+                }
+            }
+            else{
+                console.log("Error! Se esperaba: Instruccion");
+                process.exit(1);
+            } 
+        }
+
+
+    parser();
+}
+
+
